9edb6e13ee12699406b42c7dc91f78760843c9ac
Konstantin Kolev <konstantin.kolevv@gmail.com>
2020-02-25 17:45:47 +0200

    Added the FlightService class

---
+++ FlightManager/FlightManager/Services/FlightService.cs
@@ -0,0 +1,62 @@
+﻿using FlightManager.Data;
+using FlightManager.Data.Entities;
+using FlightManager.Models.Flight;
+using FlightManager.Services.Contracts;
+using FlightManager.Services.Mappings;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FlightManager.Services
+{
+    public class FlightService : IFlightService
+    {
+        private readonly FlightManagerDbContext context;
+
+        public FlightService(FlightManagerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddFlight(FlightInputModel model)
+        {
+            var flight = model.To<Flight>();
+            context.Flights.Add(flight);
+            context.SaveChanges();
+        }
+
+        public IQueryable<FlightViewModel> GetAllFlights()
+        {
+            return context.Flights.Select(x => x.To<FlightViewModel>());
+        }
+
+        public IQueryable<FlightViewModel> GetAllFlights(Expression<Func<Flight, bool>> predicate)
+        {
+            return context.Flights.Where(predicate).Select(x => x.To<FlightViewModel>());
+        }
+
+        public Flight GetOneFlight(int id)
+        {
+            return context.Flights.Find(id);
+        }
+
+        public Flight GetOneFlight(Expression<Func<Flight, bool>> predicate)
+        {
+            return context.Flights.Find(predicate);
+        }
+
+        public void RemoveFlight(int id)
+        {
+            Flight flight = context.Flights.Find(id);
+            context.Flights.Remove(flight);
+            context.SaveChanges();
+        }
+
+        public void UpdateFlight(FlightEditInputModel model)
+        {
+            Flight flight = model.To<Flight>();
+            context.Flights.Update(flight);
+            context.SaveChanges();
+        }
+    }
+}
